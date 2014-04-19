e0a3d87821243ccd152199340175cf6123ccc289
Live session user <ubuntu@ubuntu.(none)>
2014-04-19 12:20:08 +0200

    Début de l'affichage de Grid2D (Tout est commenté)
    A faire :
    récupérer la Grid du modèle
    récupérer le Thème choisi

--- TetraTruc/src/vue/Grid2D.java
+++ TetraTruc/src/vue/Grid2D.java
@@ -3,6 +3,13 @@ package vue;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import model.Tetrominoe;
+
+
+// TO DO : récupérer les infos de Grid pour squareNumberW et squareNumberH
+// TO DO : récupérer le thème choisi depuis le menu
+
+
 public class Grid2D{
 
 	//marges autour de la grille
@@ -12,8 +19,10 @@ public class Grid2D{
 	private final int squareSize = 25;
 	
 	//Nombre de cases en largeur
-	private final int squareNumberW = 10;
-	private final int squareNumberH = 20;
+// /!\ A modifier : il faut que ça corresponde au nb de cases dans Grid	
+	private int squareNumberW = 10;
+	private int squareNumberH = 20;
+	
 	
 	public void drawGrid(Graphics g, int width, int height){
 		g.setColor(Color.RED);
@@ -42,4 +51,62 @@ public class Grid2D{
 		    destY += this.squareSize;
 		}
 	}               
+	
+/*	
+	public void paintShapes(Graphics g){
+		super.paint(g);
+		
+		int boardTop = (int) size.getHeight() - squareNumberH * squareSize;
+
+
+        for (int line = 0; line < squareNumberW; line++){
+			for (int column = 0; column < squareNumberH; column++){
+                Tetrominoe shape = shapeAt(column, squareNumberH - line - 1);
+                if (shape != Tetrominoe.No_Shape)
+                    drawBrick(g, 0 + column * squareSize,
+                               boardTop + line * squareSize, shape);
+            }
+        }
+
+        if (curPiece.getShape() != Tetrominoe.No_Shape) {
+            for (int i = 0; i < 4; ++i) {
+                int x = curX + curPiece.x(i);
+                int y = curY - curPiece.y(i);
+                drawBrick(g, 0 + x * squareSize,
+                           boardTop + (squareNumberH - y - 1) * squareSize,
+                           curPiece.getShape());
+            }
+        }
+		
+					
+	}
+	
+	// Color récupérée dans Theme
+	private void drawBrick(Graphics g, int x, int y, Tetrominoe shape){
+        
+//		Color color = theme.getColorByShape(shape) // theme à récupérer depuis quelquepart
+		
+		
+        // couleur
+        g.setColor(color);
+        g.fillRect(x + 1, y + 1, squareSize - 2, squareSize - 2);
+
+        // bordures
+        g.setColor(color.brighter());
+        g.drawLine(x, y + squareSize - 1, x, y);
+        g.drawLine(x, y, x + squareSize - 1, y);
+
+//      //lettre 
+// TO DO : fonction pour avoir directement la lettre depuis 
+        //g.drawString(shape.getTetrominoe().getBrick(TROUVER_INDEX).getLetter(), 0, 0);
+         
+        // bordures
+        g.setColor(color.darker());
+        g.drawLine(x + 1, y + squareSize - 1,
+                         x + squareSize - 1, y + squareSize - 1);
+        g.drawLine(x + squareSize - 1, y + squareSize - 1,
+                         x + squareSize - 1, y + 1);
+
+    }
+*/	
 }
