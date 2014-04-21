36699dc154df2ca56ba679a1728d9e18f9576084
Baptiste Moizard <baptiste-moizard@orange.fr>
2014-04-21 19:38:34 +0200

    Calcul de vitesse du jeu en fonction du niveau

--- TetraTruc/src/model/Level.java
+++ TetraTruc/src/model/Level.java
@@ -10,6 +10,18 @@ public class Level {
 		speed = 1000;
 	}
 	
+	// Getters / Setters
 	public int getLevel(){ return level; }
+	public void setLevel(int l){ level=l; calcSpeed(); }
 	public int getSpeed(){ return speed; }
+	
+	// Fonction de calcul de la vitesse en fonction du niveau
+	private void calcSpeed(){ speed = 1000 - level*45; }
+	
+	// Passer au niveau superieur
+	public void up(){
+		if(level<20)
+			level++;
+		calcSpeed();
+	}
 }
