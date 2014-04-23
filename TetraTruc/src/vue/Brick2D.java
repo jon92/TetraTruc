1714449c0261743a7960a2fd00ae3c465a30914f
Baptiste Moizard <baptiste-moizard@orange.fr>
2014-04-23 15:00:08 +0200

    Brick2D.draw corrigée pour dessiner la brique au bon endroit dans la grid

--- TetraTruc/src/vue/Brick2D.java
+++ TetraTruc/src/vue/Brick2D.java
@@ -33,20 +33,23 @@ public class Brick2D extends JPanel {
 	public String getLetter(){ return letter; }
 	public void setLetter(String l){ letter = l; }
 	
-	public void draw(){
+	public void draw(int line, int col){
 		// cette ligne à définir, j'ai pas tout compris
 		Graphics2D g = (Graphics2D) super.getGraphics();
+        
+		int x = col*pxlWidth;
+		int y = line*pxlHeight;
 		
-		g.setColor(color);
-        g.fillRect(1, 1, pxlWidth-2, pxlHeight-2);
+        g.setColor(color);
+        g.fillRect(x+1, y+1, pxlWidth-2, pxlHeight-2);
 
         g.setColor(color.brighter());
-        g.drawLine(0, pxlHeight-1, 0, 0);
-        g.drawLine(0, 0, pxlWidth-1, 0);
+        g.drawLine(x, y+pxlHeight-1, x, y);
+        g.drawLine(x, y, x+pxlWidth-1, y);
 
         g.setColor(color.darker());
-        g.drawLine(1, pxlHeight-1, pxlWidth-1, pxlHeight-1);
-        g.drawLine(pxlWidth-1, pxlHeight-1, pxlWidth-1, 1);
+        g.drawLine(x+1, y+pxlHeight-1, x+pxlWidth-1, y+pxlHeight-1);
+        g.drawLine(x+pxlWidth-1, y+pxlHeight-1, x+pxlWidth-1, y+1);
 	}
 	
 	public static void main(String[] args) {
