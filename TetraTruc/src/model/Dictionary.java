e1279a8a796c800a3ab38a27fc33eca60def33f5
vilal <lalandev@free.fr>
2014-04-25 15:16:13 +0200

    Ajout de l'attribut path dans le dictionnaire

--- TetraTruc/src/model/Dictionary.java
+++ TetraTruc/src/model/Dictionary.java
@@ -1,13 +1,15 @@
 package model;
 
 public enum Dictionary {
-	FR("/media/lang/dictionary_FR.txt", "Français");
+	FR("media/lang/dictionary_FR.txt", "Français");
 	
+        private String path;
 	private String language;		// Langue du dictionnaire
 	private String freqLetters;		// Fréquence d'apparition des lettres dans la langue du dictionnaire
 	
 	// Constructeur
 	Dictionary(String path, String language){
+                this.path = path;
 		// Charger le dictionnaire
 		this.language = language;
 	}
@@ -15,4 +17,9 @@ public enum Dictionary {
 	// Getters
 	public String getLanguage(){ return language; }
 	public String getFreqLetters(){ return freqLetters; }
+        public String getPath(){ return path; }
+
+    String FR(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
