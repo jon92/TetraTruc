aa53bf44fabee9a779ff21be104f06f02afc4ebc
vilal <lalandev@free.fr>
2014-05-10 23:41:54 +0200

    (? pb git ?) NoAvailablePrint

---
+++ TetraTruc/src/vue/NoAvailablePrint.java
@@ -0,0 +1,24 @@
+package vue;
+
+import javax.swing.JOptionPane;
+
+public class NoAvailablePrint extends JOptionPane{
+	//private JOptionPane errorMsg;
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoAvailablePrint() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void alertNoAvailable(){
+		this.showMessageDialog(this,"Menu non disponible. Désolés! A la place on vous offre des chouquettes!");
+	}
+	
+
+}
