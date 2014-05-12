08af2250b741ce7d434c88934dee0305221dcca6
vilal <lalandev@free.fr>
2014-05-12 12:59:04 +0200

    Minor: modif de la pause (boucle for faite autrement)

--- TetraTruc/src/controleur/ContextManager.java
+++ TetraTruc/src/controleur/ContextManager.java
@@ -137,12 +137,13 @@ public class ContextManager {
 	
 	// Etat pause
 	public void setPauseState(){
+		HashMap<String, String> params = graphicEngine.getGameParams();
 		
 		if (this.gameEngine.getState() == GameState.GAME ){
 		
 			System.out.println("Pause");
 	
-			for (int i = 0; i<this.gameEngine.getBoards().size(); ++ i ){
+			for(int i =0; i< Integer.parseInt(params.get("players")); ++i){
 				this.gameEngine.getBoard(i).pause();
 			}
 			this.gameEngine.setState("PAUSE");
@@ -151,7 +152,7 @@ public class ContextManager {
 		else if (this.gameEngine.getState() == GameState.PAUSE ){
 			System.out.println("Fin de la pause");
 		
-			for (int i = 0; i <this.gameEngine.getBoards().size(); ++ i ){
+			for(int i =0; i< Integer.parseInt(params.get("players")); ++i){
 				this.gameEngine.getBoard(i).restart();
 			}
 			
