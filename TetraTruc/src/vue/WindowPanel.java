4f4d663cf47abd75dd2086a7669d1313d79136c7
vilal <lalandev@free.fr>
2014-05-12 17:07:30 +0200

    Création de la classe WindowPanel
    Jpanel spécial pour la classe Window

---
+++ TetraTruc/src/vue/WindowPanel.java
@@ -0,0 +1,28 @@
+package vue;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class WindowPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	private BufferedImage background;
+
+	public void paintComponent (Graphics g){
+		super.paintComponents(g);
+		System.out.println(background);
+		g.drawImage(background, 0, 0, null);
+			
+	}
+	
+	public BufferedImage getBackgroundImage(){
+		return this.background;
+	}
+	
+	public void setBackgroundImage(BufferedImage bg2){
+		this.background = bg2;
+		this.repaint();
+	}
+}
